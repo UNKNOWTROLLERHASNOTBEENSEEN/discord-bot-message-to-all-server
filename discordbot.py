af2365076c5a74f85f4d19317fec9e329c4c8e13
UNKNOWTROLLERHASNOTBEENSEEN <115999637+UNKNOWTROLLERHASNOTBEENSEEN@users.noreply.github.com>
2023-07-31 07:25:10 +0400

    Update and rename testcl.py to discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,6 +15,6 @@ async def send_to_all_servers(ctx, *, message):
     for guild in bot.guilds:
         for channel in guild.text_channels:
             if channel.permissions_for(guild.me).send_messages:
-                await channel.send("this is my message sub to me https://www.youtube.com/@BANDIT50010")
+                await channel.send("what sup dude") #change the message as you like it
 
-bot.run('MTEwNjgxODI4NTYxOTY0MjM3OQ.GjIZCw.Xpu-YgBjGzZ_MwuTgMBksSuwev0cfnXLdbgGEQ')
+bot.run('') #put the bot token
